8a212ca9bf7ebd5f6a42c13672fc07a2f8e5e8e2
Lovie299 <jovitanatukuda13@gmail.com>
2024-12-16 23:59:35 +0300

    Update db.php

--- Team6_MaternalTracker/db.php
+++ Team6_MaternalTracker/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'mtdb.cvgeuswekt15.eu-north-1.rds.amazonaws.com';
 $db = 'mtdb'; // Your database name
-$user = 'admin'; // Your database username
-$pass = 'cloudteam6'; // Your database password (leave empty if using XAMPP default)
+$user = 'root'; // Your database username
+$pass = ''; // Your database password (leave empty if using XAMPP default)
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
